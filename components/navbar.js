2adca5c4156a7ebef0833053e8abf062ccf73b44
Anshul Karande <109369721+anxhlk@users.noreply.github.com>
2024-01-16 22:24:39 -0500

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,25 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-import { useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
 import Logo from "../public/images/logo.svg";
-
-function Profile() {
-  const { user, error, isLoading } = useUser();
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-  if (user) {
-    return (
-      <div>
-        <h1 className="ml-4"> Welcome {user.name}!</h1>
-        <a className="mt-8 ml-48" href="/api/auth/logout">
-          Logout
-        </a>
-      </div>
-    );
-  }
-  return <a href="/api/auth/login">Login</a>;
-}
+import Profile from "./profile";
 
 const Navbar = () => {
   return (
